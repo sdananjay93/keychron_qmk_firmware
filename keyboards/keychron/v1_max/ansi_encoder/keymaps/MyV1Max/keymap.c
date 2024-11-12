0a6f93663da2fa043e75ada66fdf64fa77083539
Dananjay-Laptop <sdananjay93@gmail.com>
2024-11-12 19:44:21 +0000

    Tap Hold Functionality working CapsLock

--- keyboards/keychron/v1_max/ansi_encoder/keymaps/MyV1Max/keymap.c
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/MyV1Max/keymap.c
@@ -16,6 +16,7 @@
 
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
+#include "print.h"
 
 enum layers {
     UNIX_BASE,
@@ -27,6 +28,9 @@ enum layers {
     LAYER4,
     LAYER5,
 };
+
+#define FN_LAY MO(FN_LAYER)
+#define CP_LK LT(LAYER2, KC_NO)
 
 // └ ┌ ┬ ┴ ┤ ─ ┐ ┘ │ ┼
 
@@ -44,52 +48,59 @@ enum layers {
 // │ CTR │ OPT  │ CMD  │               SPACE                    │ CMD  │  FN  │ CTRL │ LFT  │  DN  │ RGHT │
 // └─────┴──────┴──────┴────────────────────────────────────────┴──────┴──────┴──────┴──────┴──────┴──────┘
 
+enum custom_keycodes {
+    KC_M_U = SAFE_RANGE,
+    KC_M_D,
+    KC_M_L,
+    KC_M_R,
+};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [UNIX_BASE] = LAYOUT_ansi_82(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,       KC_F12,   KC_DEL,             KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,      KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,      KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,      KC_ENT,                       DB_TOGG,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,      KC_RSFT,             KC_UP,
-        KC_LCTL,  KC_LGUI, KC_LALT,                               KC_SPC,                                   KC_RALT,  MO(FN_LAYER),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,       KC_F12,    KC_DEL,      KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,      KC_EQL,    KC_BSPC,     KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,      KC_RBRC,   KC_BSLS,     KC_PGDN,
+        CP_LK,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,      KC_ENT,                 DB_TOGG,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,      KC_UP,
+        KC_LCTL,  KC_LGUI,  KC_LALT,                KC_SPC,                              KC_RALT,  FN_LAY, KC_RCTL,           KC_LEFT, KC_DOWN,KC_RGHT
         ),
 
+
     [FN_LAYER] = LAYOUT_ansi_82(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,           RGB_TOG,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_END,
-        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,      KC_VOLU,  _______,                  RGB_TOG,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,                  _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,                  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,      _______,                            KC_END,
+        _______,  _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______, _______,   _______,  _______,      _______,
+        _______,  _______,  _______,                _______,                             _______, _______, _______,           _______, _______,_______
         ),
 
     [MAC_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,       KC_F12,   KC_DEL,             KC_MUTE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,      KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,      KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,      KC_ENT,                       KC_HOME,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,      KC_RSFT,             KC_UP,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD,                                KC_SPC,                                 KC_RCMMD, MO(FN_LAYER),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+        CP_LK,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,      KC_ENT,                       KC_HOME,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,      KC_RSFT,             KC_UP,
+        KC_LCTL,  KC_LOPTN, KC_LCMMD,               KC_SPC,                                 KC_RCMMD, MO(FN_LAYER),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
         ),
-
+    // Movement Layer
     [LAYER1] = LAYOUT_ansi_82(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  BAT_LVL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______
         ),
-
+    //  Experimentation Layer
     [LAYER2] = LAYOUT_ansi_82(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  _______,  BAT_LVL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______
+        _______,  _______,  _______,                                BAT_LVL,                                _______,  _______,  _______,  _______,  _______,  _______
         ),
     [LAYER3] = LAYOUT_ansi_82(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
@@ -157,20 +168,31 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 //     [SAMPLE_COMBO] = COMBO_ACTION(sample_combo),
 // };
 
+// Leader Key processing for layer changes
+void leader_start_user(void) {
+    // Do Something when Leader starts
+    uprintf("Leader Key Started");
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // static uint16_t lead_layer_timer;
     if (!process_record_keychron_common(keycode, record)) {
         return false;
     }
-    return true;
-
-    // Adding My Custom Handling
     switch (keycode) {
-        case KC_LCTL:
-            if (record->event.pressed && record->tap.count) {
-                tap_code16(QK_LEAD);
-                return false;
+        case CP_LK:
+            if (record->tap.count > 0) { // Key Is being tapped
+                if (record->event.pressed) { // On Tap
+                    leader_start();
+                }
+            } else {
+                if (record->event.pressed) {
+                    layer_invert(LAYER2);
+                } else {
+                    layer_invert(LAYER2);
+                }
             }
-            break;
+            return false;
     }
     return true;
 }
@@ -206,26 +228,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //     }
 // }
 
-// Leader Key processing for layer changes
-void leader_start_user(void) {
-    // Do Something when Leader starts
-}
+
 
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_K)) {
+        uprintf("Yes it is working");
         SEND_STRING("Leader Keys are working");
-    } else if (leader_sequence_one_key(KC_M)) {
-        SEND_STRING("Yes They are working");
-    } else if (leader_sequence_one_key(KC_L, KC_1)) {
-        layer_invert(LAYER1);
-    } else if (leader_sequence_one_key(KC_L, KC_2)) {
-        layer_invert(LAYER2);
-    } else if (leader_sequence_one_key(KC_L, KC_3)) {
-        layer_invert(LAYER3);
-    } else if (leader_sequence_one_key(KC_L, KC_4)) {
-        layer_invert(LAYER4);
-    } else if (leader_sequence_one_key(KC_L, KC_5)) {
-        layer_invert(LAYER5);
     }
 }
 
